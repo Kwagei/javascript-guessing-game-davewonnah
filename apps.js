a2d65dc1b3f0978b3899db3621e8c1bc8072a64c
Dave Wonnah <wonnahdave@gmail.com>
2023-04-05 16:07:57 +0000

    working on button's functionality

---
+++ apps.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.btn')
+const restart = document.querySelector('#restartGame')
+const live = document.querySelector('.lives')
+let live_num = document.querySelector('.live_num')
+
+
+restart.addEventListener("click", function(event){
+    
+    live.innerHTML = 7;
+    btn.innerHTML = ""
+
+    for (let i = 1; i <= 250; i++) {
+        const button = document.createElement("button");
+        btn.appendChild(button);
+    }
+
+    
+
+
+    
+})
+
+
+
+          
+
+ 
