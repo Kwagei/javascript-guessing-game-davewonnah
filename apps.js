6956c22b3fbf349f715984bbc72112072e90aa5f
Dave Wonnah <wonnahdave@gmail.com>
2023-04-07 12:43:30 +0000

    completed generating random numders

--- apps.js
+++ apps.js
@@ -1,32 +1,44 @@
-const btn = document.querySelector('.btn')
-const restart = document.querySelector('#restartGame')
-const live = document.querySelector('.lives')
-let live_num = document.querySelector('.live_num')
+const btn = document.querySelector('.btn');
+const restart = document.querySelector('#restartGame');
+const live = document.querySelector('.lives');
+let live_num = document.querySelector('.live_num');
+let num = [];
+
+for (let i = 1; i <= 250; i++) {
+    num.push(i);
+  }
 
 
-restart.addEventListener("click", function(event){
+restart.addEventListener("click", function(){
     
     live.innerHTML = 7;
-    btn.innerHTML = ""
-
-    for (let i = 1; i <= 250; i++) {
-        const button = document.createElement("button");
-        btn.appendChild(button);
-    }
+    btn.innerHTML = "";
 
     const div_txt = document.querySelector('.txt-area');
     div_txt.style.display = 'none';
+    // num.length = 0;
+    addnum ();
 
-    
-
-
-
-
-    
+   
+           
 })
+     
+const numArray = [];
 
-
-
-          
-
+function addnum() {
+  
+    for (let i = 0; i < 250; i++) {
+      const button = document.createElement("button");
+      const randomNumber = Math.floor(Math.random() * num.length);
+      numArray.push(num[randomNumber]);
+      button.textContent = num[randomNumber];
+      num.splice(randomNumber,1);
+      btn.appendChild(button);
+    
+    }
+  
+    console.log(numArray);
+    console.log(num);
+  }
+  
  
